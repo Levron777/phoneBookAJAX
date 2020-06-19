f4f5a88bf6f8d02c0f824f0da6612fb41a8b19fa
Levron777 <SAF2704@gmail.com>
2020-06-19 18:47:39 +0700

    Add files via upload

--- controller.php
+++ controller.php
@@ -1,21 +1,27 @@
 <?php
 require_once "./config/config.php";
 
+try {
+    $db = new PDO($dsn, USER, PASSWORD, $opt);
+
+    //Read data from the DB
+
+     if($_POST['page']) {
+         $sql = $db->prepare("SELECT * FROM `phones`");
+         $sql->execute();
+         $dataFromDB = $sql->fetchAll();
+         echo json_encode($dataFromDB);
+     }
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+
 //add data in the DB
 
 if (!$_POST['edit'] && isset($_POST['inputName']) && isset($_POST['inputPhone'])) {
     $sql = ("INSERT INTO `phones`(`name`, `phone`) VALUES(?,?)");
     $query = $db->prepare($sql);
     $query->execute([$_POST['inputName'], $_POST['inputPhone']]);
-}
-
-//Read data from the DB
-
-if ($_POST['page']) {
-    $sql = $db->prepare("SELECT * FROM `phones`");
-    $sql->execute();
-    $dataFromDB = $sql->fetchAll();
-    echo json_encode($dataFromDB);
 }
 
 //delete data from the DB
